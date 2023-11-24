5136fe331b9c4fbf425231f2e61ba78a4a65241c
AhshanHabib26 <habib.net27@gmail.com>
2023-11-24 23:18:27 +0600

    ESLint Setup

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express, { Application, Request, Response } from "express";
 
 const app: Application = express();
 
+
+
 app.get("/", (req: Request, res: Response) => {
   res.send({
     success: true,
