d5ccc714f6a2dbb1d0ef7b3b5622094bbc514c16
AhshanHabib26 <habib.net27@gmail.com>
2023-11-25 00:00:52 +0600

    Interface & Model Created

---
+++ src/modules/user.interface.ts
@@ -0,0 +1,26 @@
+
+export interface IOrder {
+    productName: string;
+    price: number;
+    quantity: number;
+  }
+  
+  export interface IUser {
+    userId: number;
+    username: string;
+    password: string;
+    fullName: {
+      firstName: string;
+      lastName: string;
+    };
+    age: number;
+    email: string;
+    isActive: boolean;
+    hobbies: string[];
+    address: {
+      street: string;
+      city: string;
+      country: string;
+    };
+    orders?: IOrder[];
+  }
---
+++ src/modules/user.model.ts
@@ -0,0 +1,79 @@
+import mongoose, { Schema } from 'mongoose';
+import { IOrder, IUser } from './user.interface';
+
+const orderSchema = new Schema<IOrder>({
+  productName: {
+    type: String,
+    required: [true, 'Product name is required'],
+  },
+  price: {
+    type: Number,
+    required: [true, 'Price is required'],
+  },
+  quantity: {
+    type: Number,
+    required: [true, 'Quantity is required'],
+  },
+});
+
+const userSchema = new Schema<IUser>({
+  userId: {
+    type: Number,
+    required: [true, 'ID is required'],
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: [true, 'Name is required'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+  },
+  fullName: {
+    firstName: {
+      type: String,
+      required: [true, 'FirstName is required'],
+    },
+    lastName: {
+      type: String,
+      required: [true, 'LastName is required'],
+    },
+  },
+  age: {
+    type: Number,
+    required: [true, 'Age is required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+  hobbies: {
+    type: [String],
+    required: [true, 'Hobbies is required'],
+  },
+  address: {
+    street: {
+      type: String,
+      required: [true, 'Street name is required'],
+    },
+    city: {
+      type: String,
+      required: [true, 'City name is required'],
+    },
+    country: {
+      type: String,
+      required: [true, 'Country name is required'],
+    },
+  },
+  orders: {
+    type: [orderSchema],
+    required: false,
+  },
+});
+
+export const User = mongoose.model<IUser>('User', userSchema);
